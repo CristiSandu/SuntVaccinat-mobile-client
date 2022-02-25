55a6d13f5660794a9ffbdea45f3db02bd630c737
CristiSandu <crystisandu3@gmail.com>
2022-02-25 23:09:24 +0200

    Changes :  - add fontAwsome  - move images in Resources

--- suntvaccinat/suntvaccinat/AssemblyInfo.cs
+++ suntvaccinat/suntvaccinat/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("fa_regular_400.ttf", Alias = "FAR")]
+[assembly: ExportFont("fa_solid_900.ttf", Alias = "FAS")]
+[assembly: ExportFont("fa_brands_400.ttf", Alias = "FAB")] 
