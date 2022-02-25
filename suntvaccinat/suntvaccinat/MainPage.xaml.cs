ffa633622bfb6b1de0069468c9f181533a2e2b25
CristiSandu <crystisandu3@gmail.com>
2022-02-25 22:16:38 +0200

    Add project files.

---
+++ suntvaccinat/suntvaccinat/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace suntvaccinat
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OrganiserBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClientBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
