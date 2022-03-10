e0197c645a2fae40a09bff1734b957bd262ad1b4
Ilie-Cristian SANDU <ilie_cristian.sandu@stud.acs.upb.ro>
2022-03-10 23:14:36 +0200

    Change :  - add UI for new user page

--- suntvaccinat/suntvaccinat/Views/Client/ProfilePage.xaml.cs
+++ suntvaccinat/suntvaccinat/Views/Client/ProfilePage.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,11 +13,28 @@ namespace suntvaccinat.Views.Client
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
+        public bool IsShown { get; set; } = false;
+
+        public string PhoneModel { get; set; }
         public ProfilePage()
         {
             InitializeComponent();
-            string deviceIdentifier = DependencyService.Get<IDevice>().GetIdentifier();
-            TestIMEI.Text = deviceIdentifier;
+            var device = DeviceInfo.Model;
+            var manufacturer = DeviceInfo.Manufacturer;
+            PhoneModel = $"{manufacturer} - {device}";
+
+            BindingContext = this;
+
+            //string deviceIdentifier = DependencyService.Get<IDevice>().GetIdentifier();
+            //TestIMEI.Text = deviceIdentifier;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            IsShown = !IsShown;
+            ButtonBigQr.IsVisible = IsShown;
+            QRCode3.IsVisible = IsShown;
+            GreenPassButton.IsVisible = !IsShown;
         }
     }
 }
