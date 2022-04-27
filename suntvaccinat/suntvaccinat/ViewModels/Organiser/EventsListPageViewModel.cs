4d5734b99794f761c1aae360c93593d649abd20f
Ilie-Cristian SANDU <ilie_cristian.sandu@stud.acs.upb.ro>
2022-04-27 17:41:49 +0300

    Changes:  - add MVVM on events page

---
+++ suntvaccinat/suntvaccinat/ViewModels/Organiser/EventsListPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using suntvaccinat.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace suntvaccinat.ViewModels.Organiser
+{
+    public class EventsListPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<EventModel> MyEvents { get; set; } = new ObservableCollection<EventModel>();
+        public Command<Models.EventModel> DeleteEvent { get; private set; }
+        public ICommand CreateEvent { get; set; }
+
+        Services.IEventsDataBase _eventsDataBase;
+
+        public EventsListPageViewModel()
+        {
+            _eventsDataBase = DependencyService.Get<Services.IEventsDataBase>();
+            PopulateEventsList();
+            DeleteEvent = new Command<Models.EventModel>(async model =>
+            {
+                MyEvents.Remove(model);
+                await _eventsDataBase.RemoveEvent(model.id_event);
+            });
+
+            CreateEvent = new Command(async model =>
+            {
+                string result = await App.Current.MainPage.DisplayPromptAsync("Add Event", "Name of Event: ");
+                if (result != null)
+                {
+                    await _eventsDataBase.AddEvent(result, DateTime.Now);
+                    PopulateEventsList();
+                }
+            });
+        }
+
+        public async void PopulateEventsList()
+        {
+            var eventsList = await _eventsDataBase.GetEvents(Helpers.DataBaseQuerys.GetEventsOrderByDate);
+            var events = new List<EventModel>(eventsList);
+            events.Reverse();
+            MyEvents = new ObservableCollection<EventModel>(events);
+            OnPropertyChanged(nameof(MyEvents));
+        }
+    }
+}
--- suntvaccinat/suntvaccinat/Views/Organiser/EventsListPage.xaml.cs
+++ suntvaccinat/suntvaccinat/Views/Organiser/EventsListPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using suntvaccinat.Models;
 using suntvaccinat.ViewModels;
+using suntvaccinat.ViewModels.Organiser;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -15,20 +16,10 @@ namespace suntvaccinat.Views.Organiser
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EventsListPage : ContentPage
     {
-        Services.IEventsDataBase _eventsDataBase;
         public EventsListPage()
         {
             InitializeComponent();
-            _eventsDataBase = DependencyService.Get<Services.IEventsDataBase>();
-            BindingContext = new EventsViewModel();
-        }
-
-        protected async override void OnAppearing()
-        {
-            base.OnAppearing();
-            var list = new List<EventModel>(await _eventsDataBase.GetEvents(Helpers.DataBaseQuerys.GetEventsOrderByDate));
-            (BindingContext as EventsViewModel).MyEvents = new ObservableCollection<Models.EventModel>(list.Reverse<EventModel>());
-            eventsList.ItemsSource = (BindingContext as EventsViewModel).MyEvents;
+            BindingContext = new EventsListPageViewModel();
         }
 
         private async void eventsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -36,32 +27,18 @@ namespace suntvaccinat.Views.Organiser
             if (e.CurrentSelection.Count == 0)
                 return;
             
-            EventModel eventt = e.CurrentSelection.FirstOrDefault() as EventModel;
+            EventModel ev = e.CurrentSelection.FirstOrDefault() as EventModel;
             if (e.CurrentSelection != null)
             {
-                await Navigation.PushAsync(new PersonEventList(eventt.Name, eventt.id_event));
+                await Navigation.PushAsync(new PersonEventList(ev.Name, ev.id_event));
             }
 
             ((CollectionView)sender).SelectedItem = null;
         }
 
-        private async void buttonEvent_Clicked(object sender, EventArgs e)
-        {
-            string result = await DisplayPromptAsync("Add Event", "Name of Event: ");
-            if (result != null)
-                await _eventsDataBase.AddEvent(result, DateTime.Now);
-            OnAppearing();
-        }
-
         private async void backBtn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
-        }
-
-        private async void SwipeItem_Clicked(object sender, EventArgs e)
-        {
-            EventModel eventModel = (EventModel)sender;
-            await _eventsDataBase.RemoveEvent(eventModel.id_event);
         }
     }
 }
