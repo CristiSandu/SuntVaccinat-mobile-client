89cb508f91ed0e492977a049ada6413f02d5f81b
Ilie-Cristian SANDU <ilie_cristian.sandu@stud.acs.upb.ro>
2022-07-02 00:29:45 +0300

    Changes :  - add remove functionlity on user, gp and insp

--- suntvaccinat/suntvaccinat/Views/Client/ProfilePage.xaml.cs
+++ suntvaccinat/suntvaccinat/Views/Client/ProfilePage.xaml.cs
@@ -24,8 +24,8 @@ namespace suntvaccinat.Views.Client
         {
             await TopLabel.TranslateTo(0, 0, 0, Easing.Linear);
             await MainCodeQR.ScaleTo(1, 250, Easing.CubicOut);
-            await MainCodeQR.ScaleTo(1.05,250,Easing.CubicOut);
-            await TopLabel.TranslateTo(0,-20,250, Easing.Linear);
+            await MainCodeQR.ScaleTo(1.05, 250, Easing.CubicOut);
+            await TopLabel.TranslateTo(0, -20, 250, Easing.Linear);
         }
 
         private void Button_Clicked(object sender, EventArgs e)
@@ -36,6 +36,32 @@ namespace suntvaccinat.Views.Client
         private async void ToolbarItem_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new SettingsPage());
+        }
+
+        private async void ToolbarItem_Clicked_1(object sender, EventArgs e)
+        {
+            SecureStorage.Remove(Helpers.Constants.INSPPref);
+            Preferences.Remove(Helpers.Constants.INSPPref);
+            await Navigation.PopToRootAsync();
+
+        }
+
+        private async void ToolbarItem_Clicked_2(object sender, EventArgs e)
+        {
+            SecureStorage.Remove(Helpers.Constants.GreenPass);
+            Preferences.Remove(Helpers.Constants.GreenPass);
+            await Navigation.PopToRootAsync();
+
+        }
+
+        private async void ToolbarItem_Clicked_3(object sender, EventArgs e)
+        {
+            SecureStorage.Remove(Helpers.Constants.User);
+            SecureStorage.Remove(Helpers.Constants.PhoneNumber);
+            Preferences.Remove(Helpers.Constants.User);
+            Preferences.Remove(Helpers.Constants.INSPPref);
+            Preferences.Remove(Helpers.Constants.GreenPass);
+            await Navigation.PopToRootAsync();
         }
     }
 }
